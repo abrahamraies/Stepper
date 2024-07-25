855a110844abc5af715ffd8bf1a9a0939f77374a
abrahamraies <abrahamraies@gmail.com>
2024-07-25 13:02:21 +1000

    Project creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,51 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'StepperForm';
+
+  tryOutLanguages: boolean = false;
+
+  stepsList: any [] = [
+     { stepName: 'Choose a language.', isCompleted: false },
+     { stepName: 'Thinking or looking for a project.', isCompleted: false },
+     { stepName: 'Start developing.', isCompleted: false },
+  ];
+  activeStep: any = this.stepsList[0];
+
+  stepperCompletionValue: number = 8; // 8% is the default value for the progress bar
+
+  changeTheme(theme:string)
+  {
+    const body=document.body as HTMLElement
+    body.setAttribute('data-bs-theme',theme)
+  }
+
+  setActiveStep(stepSelected: any) {
+    this.activeStep = stepSelected;
+    switch(stepSelected.stepName) {
+      case 'Choose a language.':
+        this.stepperCompletionValue = 8;
+        this.stepsList[0].isCompleted = false;
+        this.stepsList[1].isCompleted = false;
+        break;
+      case 'Thinking or looking for a project.':
+        this.stepperCompletionValue = 50;
+        this.stepsList[0].isCompleted = true;
+        this.stepsList[1].isCompleted = false;
+        console.log(this.stepsList[0]);
+        break;
+      case 'Start developing.':
+        this.stepperCompletionValue = 90;
+        this.stepsList[0].isCompleted = true;
+        this.stepsList[1].isCompleted = true;
+        break;
+    }
+  }
+
+  setActiveLanguage() {
+    if(this.tryOutLanguages) {
+    this.tryOutLanguages = false;
+    } else {
+      this.tryOutLanguages = true;
+    }
+  }
 }
